20af3f349c681154f225ebb5eadfd09a99e73da5
Yuriy Astrov <yuriastrov@gmail.com>
2015-01-19 00:57:20 +0300

    DiaryRuClient: Now JSON works good, but  not works because I must see for arrays and arrays of pointers.

--- DiaryRuAPIClient.go
+++ DiaryRuAPIClient.go
@@ -15,7 +15,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/disintegration/charmap"
+	//"github.com/disintegration/charmap"
+	"errors"
+	"github.com/yastrov/charmap"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -48,7 +50,7 @@ func (this *DiaryAPIClient) Init() {
 	this.HttpClient = &http.Client{}
 }
 
-func (this *DiaryAPIClient) Auth(user, password string) {
+func (this *DiaryAPIClient) Auth(user, password string) error {
 	strcp1251, _ := charmap.Encode(user, "cp-1251")
 
 	values := url.Values{}
@@ -59,20 +61,21 @@ func (this *DiaryAPIClient) Auth(user, password string) {
 	values.Add("appkey", appkey)
 
 	this.URL, _ = url.Parse(DiaryMainUrl)
-	r, _ := this.dorequest(values, nil)
-	resp, _ := this.HttpClient.Do(r)
+	r, err := this.dorequest(values, nil)
+	resp, err := this.HttpClient.Do(r)
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		log.Fatal(resp.StatusCode)
+		return errors.New(resp.Status)
 	}
 	var message DiaryAPIAuthResponse
 	decoder := json.NewDecoder(resp.Body)
-	err := decoder.Decode(&message)
+	err = decoder.Decode(&message)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	this.SID = message.SID
 	this.Timestamp = time.Now()
+	return nil
 }
 
 func (this *DiaryAPIClient) testAPITime() bool {
@@ -107,59 +110,75 @@ func (this *DiaryAPIClient) dorequest(values url.Values, data []byte) (r *http.R
 }
 
 type PostStruct struct {
-	postid              string            `json:"postid"`
-	juserid             string            `json:"juserid"`
-	shortname           string            `json:"shortname"`
-	journal_name        string            `json:"journal_name"`
-	message_src         string            `json:"message_src"`
-	message_html        string            `json:"message_html"`
-	author_userid       string            `json:"author_userid"`
-	author_shortname    string            `json:"author_shortname"`
-	author_username     string            `json:"author_shortname"`
-	author_title        string            `json:"author_title"`
-	title               string            `json:"title"`
-	no_comments         string            `json:"no_comments"`         // Flag for no comments
-	comments_count_data string            `json:"comments_count_data"` //Count of comments
-	tags_data           map[string]string `json:"tags_data"`
+	Avatar_path         string            `json:"avatar_path"`
+	Postid              string            `json:"postid"`
+	Juserid             string            `json:"juserid"`
+	Shortname           string            `json:"shortname"`
+	Journal_name        string            `json:"journal_name"`
+	Message_src         string            `json:"message_src"`
+	Message_html        string            `json:"message_html"`
+	Author_userid       string            `json:"author_userid"`
+	Author_shortname    string            `json:"author_shortname"`
+	Author_username     string            `json:"author_shortname"`
+	Author_title        string            `json:"author_title"`
+	Title               string            `json:"title"`
+	No_comments         string            `json:"no_comments"`         // Flag for no comments
+	Comments_count_data string            `json:"comments_count_data"` //Count of comments
+	Tags_data           map[string]string `json:"tags_data"`
+	Subscribed          string            `json:"subscribed"`
+	Can_edit            string            `json:"can_edit"`
+	Avatarid            string            `json:"avatarid "`
+	No_smile            string            `json:"no_smile"`
+	Jaccess             string            `json:"jaccess"`
+	Dateline_cdate      string            `json:"dateline_cdate"`
+	Close_access_mode2  string            `json:"close_access_mode2"`
+	Close_access_mode   string            `json:"close_access_mode"`
+	Dateline_date       string            `json:"dateline_date"`
+	Access              string            `json:"access"`
 }
 
-func (this *DiaryAPIClient) post_get(shortname string) {
+type DiaryAPIPostGet struct {
+	Result int                    `json:"result,string"`
+	Posts  map[string]*PostStruct `json:"posts"`
+	Error  string                 `json:"error"`
+}
+
+func (this *DiaryAPIClient) post_get(shortname, type_, from string) ([]*PostStruct, error) {
 	values := url.Values{}
 	values.Add("sid", this.SID)
-	values.Add("type", "diary")
+	values.Add("type", type_)
 	values.Add("method", "post.get")
-	values.Add("shortname", shortname)
-
-	r, _ := this.dorequest(values, nil)
-	resp, _ := this.HttpClient.Do(r)
+	if shortname != "" {
+		values.Add("shortname", shortname)
+	}
+	if from != "" {
+		values.Add("from", from)
+	}
+	r, err := this.dorequest(values, nil)
+	resp, err := this.HttpClient.Do(r)
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		log.Fatal(resp.StatusCode)
+		return nil, errors.New(resp.Status)
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	fmt.Println(string(body))
-	var message map[string]json.RawMessage
+	var message DiaryAPIPostGet
 	err = json.Unmarshal(body, &message)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	//Also may try: var posts map[string]*PostStruct
-	var posts map[string]PostStruct //It's comfortable, but don't work
-	//var posts map[string]interface{} //It's work fine, but noncomfortable
-	err = json.Unmarshal(message["posts"], &posts)
-	fmt.Println(posts)
-	if err != nil {
-		log.Fatal(err)
+	result := make([]*PostStruct, len(message.Posts))
+	for id, post_unit := range message.Posts {
+		fmt.Println(id, "-", post_unit.Postid)
+		result = append(result, post_unit)
 	}
-	for id, post_unit := range posts {
-		fmt.Println(id, post_unit)
-	}
+	return result, nil
 }
 
-func (this *DiaryAPIClient) post_create(title, message string) {
+func (this *DiaryAPIClient) post_create(title, message string) (string, error) {
 	values := url.Values{}
 	values.Add("sid", this.SID)
 	values.Add("message", message)
@@ -172,27 +191,108 @@ func (this *DiaryAPIClient) post_create(title, message string) {
 	resp, err := http.PostForm(this.URL.String(), values)
 	defer resp.Body.Close()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	if resp.StatusCode != http.StatusOK {
-		log.Fatal(resp.StatusCode)
+		return "", errors.New(resp.Status)
 	}
-	/*body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}*/
 	type APIResponse struct {
 		Result  int    `json:"result,string"`
 		Error   string `json:"error,string"`
 		Message string `json:"message"`
 		PostID  string `json:"postid"`
 	}
-	var message APIResponse
+	var mess APIResponse
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&mess)
+	if err != nil {
+		return "", err
+	}
+	return mess.PostID, nil
+}
+
+type DiaryAPICommentGet struct {
+	Result   int                       `json:"result,string"`
+	Comments map[string]*CommentStruct `json:"comments"`
+	Error    string                    `json:"error"`
+}
+
+type CommentStruct struct {
+	Avatar_path      string `json:"avatar_path"`
+	Postid           string `json:"postid"` // For manually control
+	Commentid        string `json:"commentid"`
+	Shortname        string `json:"shortname"`
+	Message_html     string `json:"message_html"`
+	Author_userid    string `json:"author_userid"`
+	Author_shortname string `json:"author_shortname"`
+	Author_avatar    string `json:"author_avatar"`
+	Author_username  string `json:"author_shortname"`
+	Author_title     string `json:"author_title"`
+	Can_edit         string `json:"can_edit"`
+	Can_delete       string `json:"can_delete"`
+	Dateline         string `json:"dateline"`
+}
+
+func (this *DiaryAPIClient) comment_get(postid string) ([]*CommentStruct, error) {
+	values := url.Values{}
+	values.Add("sid", this.SID)
+	values.Add("method", "comment.get")
+	values.Add("postid", postid)
+	r, err := this.dorequest(values, nil)
+	resp, err := this.HttpClient.Do(r)
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+	var message DiaryAPICommentGet
 	dec := json.NewDecoder(resp.Body)
 	err = dec.Decode(&message)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
+	result := make([]*CommentStruct, len(message.Comments))
+	for _, comment_unit := range message.Comments {
+		comment_unit.Postid = postid
+		result = append(result, comment_unit)
+	}
+	return result, nil
+}
+
+func (this *DiaryAPIClient) comment_get_for_post(post PostStruct) ([]*CommentStruct, error) {
+	values := url.Values{}
+	values.Add("sid", this.SID)
+	values.Add("method", "comment.get")
+	values.Add("postid", post.Postid)
+	r, err := this.dorequest(values, nil)
+	resp, err := this.HttpClient.Do(r)
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+	var message DiaryAPICommentGet
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&message)
+	if err != nil {
+		return nil, err
+	}
+	result := make([]*CommentStruct, len(message.Comments))
+	for _, comment_unit := range message.Comments {
+		comment_unit.Postid = post.Postid
+		result = append(result, comment_unit)
+	}
+	return result, nil
+}
+
+func (this *DiaryAPIClient) comment_get_for_posts(posts []*PostStruct) (result []*CommentStruct, err error) {
+	//var result []*PostStruct
+	result = make([]*PostStruct, 20)
+	for post := range posts {
+		if post.Comments_count_data != "" {
+			comments, err := comment_get_for_post(*post)
+			result = append(result, comments)
+		}
+	}
+	return result, err
 }
 
 func main() {
@@ -201,7 +301,24 @@ func main() {
 	flag.Parse()
 	var diary DiaryAPIClient
 	diary.Init()
-	diary.Auth(*userPtr, *passPtr)
+	err := diary.Auth(*userPtr, *passPtr)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(diary.SID)
-	diary.post_create("Test Title", "Test message")
+	//diary.post_create("Test Title", "Test message")
+	posts, err := diary.post_get("", "diary", "0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for post := range posts {
+		fmt.Println((*post).Avatar_path)
+	}
+	comments, err := diary.comment_get()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for post := range comments {
+		fmt.Println(post.PostID)
+	}
 }
